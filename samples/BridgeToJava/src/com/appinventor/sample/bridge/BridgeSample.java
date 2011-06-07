ffd8e3e4e66504eb1e34d9a8ca255ae6485fe599
halabelson <halabelson@google.com>
2011-06-07 01:40:52 +0000

    change intendation from 4 spaces to 2

--- samples/BridgeToJava/src/com/appinventor/sample/bridge/BridgeSample.java
+++ samples/BridgeToJava/src/com/appinventor/sample/bridge/BridgeSample.java
@@ -40,117 +40,117 @@ import android.util.Log;
 // events via dispatchEvent, as shown below.
 public class BridgeSample extends Form implements HandlesEventDispatching {
 
-    private static final String LOG_KEY = "AppInvTest";
+  private static final String LOG_KEY = "AppInvTest";
 
-    private Button button1;
-    private Button button2;
-    private Label label;
-    private Notifier notifier;
-    private int clickCount = 0;
-
-
-    // The equivalent to a "main" method for App Inventor apps is the $define method.
-    void $define() {
-
-        // Component constructors take their container as an argument.  The Form is the root container
-        // for an App Inventor app.
-
-        // The Logo component lets you write messages in the system log, which can be useful 
-        // for debugging.  The output is written into the Android system log (viewable with logcat).
-        Log.i(LOG_KEY, "starting the app");
-        HorizontalArrangement ha = new HorizontalArrangement(this);
-
-        // Here we a placing a Button in the HorizontalArrangement so we pass that into the Button
-        // constructor.
-        button1 = new Button(ha);
-
-        // Set properties of the button by calling the corresponding property setters.  These
-        // correspond to the App Inventor blocks that set properties for buttons.  For detailed
-        // information, see the Button.java source code.
-        Log.i(LOG_KEY, "setting the properties for button1");
-        button1.TextColor(COLOR_RED);
-        button1.Text("Click me!");
-
-        button2 = new Button(ha);
-        Log.i(LOG_KEY, "setting the properties for button2");
-        button1.TextColor(COLOR_BLUE);
-        button1.FontBold(true);
-        button1.FontSize(16.0f);  //font size is a float
-        button2.Text("No, Click me!");
+  private Button button1;
+  private Button button2;
+  private Label label;
+  private Notifier notifier;
+  private int clickCount = 0;
 
 
-        Image image = new Image(this);
-        // The string that we pass to the Picture property must name a file in the assets
-        // directory for this Android project tree.
-        image.Picture("happyface.png");
+  // The equivalent to a "main" method for App Inventor apps is the $define method.
+  void $define() {
 
-        Log.i(LOG_KEY, "creating the label");
-        label = new Label(this);
+    // Component constructors take their container as an argument.  The Form is the root container
+    // for an App Inventor app.
 
-        // Register for events.  By the second argument can be any string.    The third argument must 
-        // exactly match the name of the event that you want to handle for that component.  When the event
-        // happens, dispatchEvent will be called with these arguments.
-        EventDispatcher.registerEventForDelegation(this, "AppInventorInJava", "Click");
+    // The Logo component lets you write messages in the system log, which can be useful 
+    // for debugging.  The output is written into the Android system log (viewable with logcat).
+    Log.i(LOG_KEY, "starting the app");
+    HorizontalArrangement ha = new HorizontalArrangement(this);
 
-        // Create a Label component, similarly as with the Button
-        label = new Label(this);
+    // Here we a placing a Button in the HorizontalArrangement so we pass that into the Button
+    // constructor.
+    button1 = new Button(ha);
 
-        // Add a Notifier, which is a non-visible component.   Non-visible components take the
-        // form as their container, just like visible components.
-        notifier = new Notifier(this);  
+    // Set properties of the button by calling the corresponding property setters.  These
+    // correspond to the App Inventor blocks that set properties for buttons.  For detailed
+    // information, see the Button.java source code.
+    Log.i(LOG_KEY, "setting the properties for button1");
+    button1.TextColor(COLOR_RED);
+    button1.Text("Click me!");
 
-        // Finish off the $define by initializing the click count and the label text. 
-        clickCount = 0;
-        showCount();
+    button2 = new Button(ha);
+    Log.i(LOG_KEY, "setting the properties for button2");
+    button1.TextColor(COLOR_BLUE);
+    button1.FontBold(true);
+    button1.FontSize(16.0f);  //font size is a float
+    button2.Text("No, Click me!");
 
+
+    Image image = new Image(this);
+    // The string that we pass to the Picture property must name a file in the assets
+    // directory for this Android project tree.
+    image.Picture("happyface.png");
+
+    Log.i(LOG_KEY, "creating the label");
+    label = new Label(this);
+
+    // Register for events.  By the second argument can be any string.    The third argument must 
+    // exactly match the name of the event that you want to handle for that component.  When the event
+    // happens, dispatchEvent will be called with these arguments.
+    EventDispatcher.registerEventForDelegation(this, "AppInventorInJava", "Click");
+
+    // Create a Label component, similarly as with the Button
+    label = new Label(this);
+
+    // Add a Notifier, which is a non-visible component.   Non-visible components take the
+    // form as their container, just like visible components.
+    notifier = new Notifier(this);  
+
+    // Finish off the $define by initializing the click count and the label text. 
+    clickCount = 0;
+    showCount();
+
+  }
+
+  // Here is the event dispatcher for our app.  We need to Override the method for the Form 
+  // superclass
+  @Override
+  public boolean dispatchEvent(Component component, String id, String eventName,
+      Object[] args) {
+    Log.i(LOG_KEY,
+        String.format("dispatchEvent called: %s, %s, %s",
+            id, eventName, component.toString()));
+    if (component.equals(button1) && eventName.equals("Click")) {
+      button1WasClicked();
+      return true;
+    } else if (component.equals(button2) && eventName.equals("Click")) {
+      button2WasClicked();
+      return true;
+    } // else clauses for additional events would go here
+    return false;
+  }
+
+  public void button1WasClicked() {
+    Log.i(LOG_KEY, "button1 clicked");
+    button1.Text("Button1 Was Clicked");
+    checkCount();
+  }
+
+  private void button2WasClicked() {
+    Log.i(LOG_KEY, "button2 clicked");
+    // Note that we can change properties of components other than the one that was clicked.
+    button1.Text("Button2 Was Clicked");
+    checkCount();
+  }
+
+  // Here's the code that dispatchEvent calls for handling Button clicks.
+  public void checkCount() {
+    clickCount += 1;
+    showCount();
+    if (clickCount == 6) {
+      notifier.ShowMessageDialog(
+          "No more button clicks for you!",
+          "Too many clicks",
+      "Start over");
+      clickCount = 0;
+      showCount();
     }
+  }
 
-    // Here is the event dispatcher for our app.  We need to Override the method for the Form 
-    // superclass
-    @Override
-    public boolean dispatchEvent(Component component, String id, String eventName,
-            Object[] args) {
-        Log.i(LOG_KEY,
-                String.format("dispatchEvent called: %s, %s, %s",
-                        id, eventName, component.toString()));
-        if (component.equals(button1) && eventName.equals("Click")) {
-            button1WasClicked();
-            return true;
-        } else if (component.equals(button2) && eventName.equals("Click")) {
-            button2WasClicked();
-            return true;
-        } // else clauses for additional events would go here
-        return false;
-    }
-
-    public void button1WasClicked() {
-        Log.i(LOG_KEY, "button1 clicked");
-        button1.Text("Button1 Was Clicked");
-        checkCount();
-    }
-
-    private void button2WasClicked() {
-        Log.i(LOG_KEY, "button2 clicked");
-        // Note that we can change properties of components other than the one that was clicked.
-        button1.Text("Button2 Was Clicked");
-        checkCount();
-    }
-
-    // Here's the code that dispatchEvent calls for handling Button clicks.
-    public void checkCount() {
-        clickCount += 1;
-        showCount();
-        if (clickCount == 6) {
-            notifier.ShowMessageDialog(
-                    "No more button clicks for you!",
-                    "Too many clicks",
-            "Start over");
-            clickCount = 0;
-            showCount();
-        }
-    }
-
-    public void showCount() {
-        label.Text("You clicked " + clickCount + " times");
-    }
+  public void showCount() {
+    label.Text("You clicked " + clickCount + " times");
+  }
 }
